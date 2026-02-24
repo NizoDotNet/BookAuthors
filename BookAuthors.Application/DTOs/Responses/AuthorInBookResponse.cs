76ce6b123d1dab21a4d3a6f0a9989d40cdc47290
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-02-24 16:42:54 +0400

    minor changes

--- BookAuthors.Application/DTOs/Responses/AuthorInBookResponse.cs
+++ BookAuthors.Application/DTOs/Responses/AuthorInBookResponse.cs
@@ -1,11 +1,9 @@
 ﻿namespace BookAuthors.Application.DTOs.Responses;
 
-public class AuthorResponse
+public class AuthorInBookResponse
 {
     public Guid Id { get; init; }
     public string Name { get; init; } = null!;
     public string Surname { get; init; } = null!;
     public string Middlename { get; init; } = string.Empty;
-    public string Bio { get; init; } = string.Empty!;
-    public List<BookResponse> Books { get; set; } = [];
 }
--- BookAuthors.Application/DTOs/Responses/BookResponseWithAuthors.cs
+++ BookAuthors.Application/DTOs/Responses/BookResponseWithAuthors.cs
@@ -1,8 +1,9 @@
 ﻿namespace BookAuthors.Application.DTOs.Responses;
 
-public class BookResponse
+public class BookResponseWithAuthors
 {
     public Guid Id { get; set; }
     public string Title { get; init; } = null!;
     public string Description { get; init; } = string.Empty;
+    public List<AuthorInBookResponse> Authors { get; set; } = [];
 }
